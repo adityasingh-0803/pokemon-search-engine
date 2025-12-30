5fe8ae7af829b5f26863ed57e8d77f77981de9a2
Aditya Singh <155234975+adityasingh-0803@users.noreply.github.com>
2025-12-30 22:30:10 +0530

    Update PokemonCard.jsx

--- frontend/src/components/PokemonCard.jsx
+++ frontend/src/components/PokemonCard.jsx
@@ -1,13 +1,32 @@
 export default function PokemonCard({ data }) {
   return (
-    <div style={{ border: "1px solid #ccc", padding: 20 }}>
+    <div
+      style={{
+        border: "1px solid #ccc",
+        padding: 20,
+        marginTop: 20,
+        borderRadius: 8,
+        maxWidth: 400
+      }}
+    >
       <h2>{data.name.toUpperCase()}</h2>
-      <img src={data.sprite} />
-      <p>Types: {data.types.join(", ")}</p>
-      <p>Abilities: {data.abilities.join(", ")}</p>
+
+      <img src={data.sprite} alt={data.name} />
+
+      <p>
+        <b>Types:</b> {data.types.join(", ")}
+      </p>
+
+      <p>
+        <b>Abilities:</b> {data.abilities.join(", ")}
+      </p>
+
+      <h4>Stats</h4>
       <ul>
-        {Object.entries(data.stats).map(([k, v]) => (
-          <li key={k}>{k}: {v}</li>
+        {Object.entries(data.stats).map(([key, value]) => (
+          <li key={key}>
+            {key}: {value}
+          </li>
         ))}
       </ul>
     </div>
